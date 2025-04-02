d75a0f82edab70ac31ce80c4394fe31b6ce95b90
Amine Viinturuth <88991558+aminerouimba@users.noreply.github.com>
2025-04-02 14:01:44 +0000

    init commit

---
+++ localSauve/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "offline-cache-v2";
+const urlsToCache = [
+    "./",  
+    "./index.html",
+    "./save.php",
+    "./app.js"
+];
+
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
